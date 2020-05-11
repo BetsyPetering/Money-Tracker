82f4ff96d588665b839b6e58db5e56d752553962
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-05-11 00:02:03 -0700

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
 const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
 
@@ -15,15 +23,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
-
 // routes
 app.use(require("./routes/api.js"));
-console.log(PORT);
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
